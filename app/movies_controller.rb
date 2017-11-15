bf5eac9164a761926583b4f5181c9b904e693f72
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-15 19:43:52 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,7 +83,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.delte(movie.id)
+  Movie.delete(movie.id)
 end
 
 def can_destroy_all_items_at_once
